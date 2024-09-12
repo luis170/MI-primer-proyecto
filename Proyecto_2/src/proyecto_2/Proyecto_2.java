6ad0c2aa48402c832293b78e1641b8ef8a4c27de
Luis1705 <lsanabria1705@gmail.com>
2024-09-12 17:29:40 -0500

    mi primer commit

---
+++ Proyecto_2/src/proyecto_2/Proyecto_2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_2;
+
+/**
+ *
+ * @author user
+ */
+public class Proyecto_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1, num2, resultado;
+        num1 = 4;
+        num2 = 6;
+        resultado = num1 + num2;
+        System.out.println("El resultado de la suma es:   " + resultado);
+    }
+    
+}
---
+++ src/programas/Programa01.java
@@ -0,0 +1,13 @@
+
+package programas;
+
+
+public class Programa01 {
+
+   
+    public static void main(String[] args) {
+        
+    System.out.println("HOLA LUIS SANABRIA");
+    }
+    
+}
